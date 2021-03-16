4f2c4de5bbce9ae2af8368bc33a48b31f3364691
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-03-16 19:15:27 -0400

    Enigma algorithm started

---
+++ lib/Algorithms/enigma_machine.dart
@@ -0,0 +1,79 @@
+void main() {
+  var LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//   var rotors = ("I","II","III");
+//   var reflector = "UKW-B";
+//   var ringSettings ="AAC";
+//   var ringPositions = "DEF" ;
+//   var plugboard = "AT BS DE FM IR KN LZ OW PV XY";
+
+  print (caesarShift("Hola", 45));
+}
+
+String caesarShift(str,amount){
+  var output="";
+  for (var i = 0; i < str.length; i++) {
+    var c=str[i];
+    var code=str.codeUnitAt(i);
+    if((code>=65)&&(code<=90)){
+      c=String.fromCharCode(((code - 65 + amount) % 26) + 65);
+    }
+    output = output + c;
+  }
+  return output;
+}
+void encode(plaintext){
+  var rotors, reflector,ringSettings,ringPositions,plugboard;
+  //Enigma Rotors and reflectors
+  var rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+  var rotor1Notch = "Q";
+  var rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+  var rotor2Notch = "E";
+  var rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+  var rotor3Notch = "V";
+  var rotor4 = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+  var rotor4Notch = "J";
+  var rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK";
+  var rotor5Notch = "Z";
+
+  var rotorDict = {"I":rotor1,"II":rotor2,"III":rotor3,"IV":rotor4,"V":rotor5};
+  var rotorNotchDict = {"I":rotor1Notch,"II":rotor2Notch,"III":rotor3Notch,"IV":rotor4Notch,"V":rotor5Notch};
+
+  var reflectorB = {"A":"Y","Y":"A","B":"R","R":"B","C":"U","U":"C","D":"H","H":"D","E":"Q","Q":"E","F":"S","S":"F","G":"L","L":"G","I":"P","P":"I","J":"X","X":"J","K":"N","N":"K","M":"O","O":"M","T":"Z","Z":"T","V":"W","W":"V"};
+  var reflectorC = {"A":"F","F":"A","B":"V","V":"B","C":"P","P":"C","D":"J","J":"D","E":"I","I":"E","G":"O","O":"G","H":"Y","Y":"H","K":"R","R":"K","L":"Z","Z":"L","M":"X","X":"M","N":"W","W":"N","Q":"T","T":"Q","S":"U","U":"S"};
+
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var rotorANotch = false;
+  var rotorBNotch = false;
+  var rotorCNotch = false;
+
+  if(reflector=="UKW-B"){
+    var reflectorDict=reflectorB;
+  }else{
+    var reflectorDirt = reflectorC;
+  }
+
+  var rotorA = rotorDict[rotors[0]];
+  var rotorB = rotorDict[rotors[1]];
+  var rotorC = rotorDict[rotors[2]];
+  // rotorANotch = rotorNotchDict[rotors[0]];
+  // rotorBNotch = rotorNotchDict[rotors[1]];
+  // rotorCNotch = rotorNotchDict[rotors[2]];
+
+  var rotorALetter = ringPositions[0];
+  var rotorBLetter = ringPositions[1];
+  var rotorCLetter = ringPositions[2];
+
+  var rotorASetting = ringSettings[0];
+  var offsetASetting = alphabet.index(rotorASetting);
+  var rotorBSetting = ringSettings[1];
+  var offsetBSetting = alphabet.index(rotorBSetting);
+  var rotorCSetting = ringSettings[2];
+  var offsetCSetting = alphabet.index(rotorCSetting);
+
+  rotorA = caesarShift(rotorA,offsetASetting);
+  rotorB = caesarShift(rotorB,offsetBSetting);
+  rotorC = caesarShift(rotorC,offsetCSetting);
+
+
+
+}
