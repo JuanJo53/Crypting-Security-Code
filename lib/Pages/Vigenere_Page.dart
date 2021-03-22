8f4c651ed6739cf355599c2d01bf23ee80e2ba82
jantonio2 <jesusrochavedia@gmail.com>
2021-03-22 00:06:30 -0400

    Vigenere_Page base done

--- lib/Pages/Vigenere_Page.dart
+++ lib/Pages/Vigenere_Page.dart
@@ -14,14 +14,146 @@ class _Vigenere_PageState extends State<Vigenere_Page>{
   Size size;
   _Vigenere_PageState(this._colores){
   }
+  TextEditingController input=TextEditingController();
+  TextEditingController number=TextEditingController();
+  TextEditingController output=TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  double heightAppbar;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
+    heightAppbar=Scaffold.of(context).appBarMaxHeight;
+    size=MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Hola"
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height-heightAppbar-60,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Input("Ingrese el texto a cifrar",input,TextInputType.text,size.width*0.1),
+              Input("Ingrese la llave",input,TextInputType.text,size.width*0.1),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+
+                  Boton("Encriptar"),
+
+                  Boton("Desencriptar"),
+                ],
+              ),
+              InputArea("Texto Cifrado",output),
+
+            ],
+          ),
         ),
+      ),
+    );
+  }
+  void cifrar(){
+    setState(() {
+      print(number.text);
+      //output.text=caesar(double.parse(number.text).toInt(), input.text);
+    });
+  }
+  void descifrar(){
+    setState(() {
+      print(number.text);
+      //output.text=caesar(double.parse(number.text).toInt()*-1, input.text);
+    });
+  }
+  Widget Boton(label){
+    return Container(
+      decoration: BoxDecoration(
+        color: _colores.secondColor,
+        borderRadius: BorderRadius.circular(size.width*0.05),
+      ),
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(size.width*0.05)),
+        onPressed: (){
+          if(label=="Encriptar"){
+            cifrar();
+          }
+          else{
+            descifrar();
+          }
+
+        },
+        child: Text(label),
+      ),
+    );
+  }
+  Widget InputArea(label,input){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: size.width*0.05),
+      child: Column(
+        children: [
+          Text(label),
+          SizedBox(
+            height: size.height*0.01,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))
+            ),
+            child:
+            TextField(
+              readOnly: true,
+              maxLines: 10,
+              controller: input,
+              cursorColor: _colores.primaryColor,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: _colores.secondColor),
+                      borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))),
+                  contentPadding: EdgeInsets.symmetric(horizontal: size.width*0.03,vertical: size.width*0.03),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: _colores.primaryColor),
+                      borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))
+                  )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  Widget Input(label,input,key,margin){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: margin),
+      child: Column(
+        children: [
+          Text(label,textAlign: TextAlign.center,),
+          SizedBox(
+            height: size.height*0.01,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))
+            ),
+            child:
+            TextField(
+
+              controller: input,
+              keyboardType: key,
+              cursorColor: _colores.primaryColor,
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: _colores.secondColor),
+                      borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))),
+                  contentPadding: EdgeInsets.symmetric(horizontal: size.width*0.03),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: _colores.primaryColor),
+                      borderRadius: BorderRadius.all(Radius.circular(size.width*0.05))
+                  )
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
